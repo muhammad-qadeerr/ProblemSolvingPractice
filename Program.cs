a139595149faa13c52719bc3f916e6ca083c4f70
Muhammad Qadeer <qadeeransari38@gmail.com>
2025-03-07 00:53:48 +0500

    Leetcode 168. Excel Sheet Column Title

--- Program.cs
+++ Program.cs
@@ -191,6 +191,36 @@ namespace ProblemSolvingPratice
             return true;
 
         }
+
+        // Leetcode 168. Excel Sheet Column Title
+
+        public static string ConvertToTitle(int columnNumber)
+        {
+
+            int remainder = 0;
+            string result = string.Empty;
+            string finalResult = string.Empty;
+            while (columnNumber > 0)
+            {
+                //Edge-case when value is 26 itself
+                columnNumber--;
+                remainder = columnNumber % 26;
+                char c = (char)(remainder + 'A');
+                result += c;
+                columnNumber = columnNumber / 26;
+            }
+            Stack<char> stk = new Stack<char>();
+
+            foreach (char c in result)
+            {
+                stk.Push(c);
+            }
+            foreach (char c in result)
+            {
+                finalResult += stk.Pop();
+            }
+            return finalResult;
+        }
         static void Main(string[] args)
         {
             string str = " qadeer ";
@@ -239,6 +269,11 @@ namespace ProblemSolvingPratice
 
             bool isValidPalindrom = IsPalindrome(strr);
             Console.WriteLine($"String: {strr} ----  is valid palindrom ? : {isValidPalindrom}");
+
+            Console.WriteLine("------------------------------------------------------------");
+
+            string coloumnTitle = ConvertToTitle(29);
+            Console.WriteLine($"coloumnTitle: {coloumnTitle}");
 
 
 
